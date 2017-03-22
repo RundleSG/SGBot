ae91140f38f46c97fca6056a29148e0db8b8e3c1
RundleSG <Rundle.r97@gmail.com>
2017-03-22 01:07:17 -0600

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+  if(message.content == 'ping') {
+    // message.reply('pong');
+    message.channel.sendMessage('pong');
+  }
+});
+
+bot.login('MjkzNjM5OTQ1OTg4NjAzOTA1.C7Oxcg.Hyctwf3FiJS7sndFZ3IYGBugtLM');
