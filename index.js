3be763c2b23a802dbc94234f71f5c5a5c8b0e8c7
RundleSG <Rundle.r97@gmail.com>
2017-03-24 21:41:01 -0600

    0.0.2

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const bot = new Discord.Client();
 
 bot.on('message', (message) => {
   if(message.content == 'ping') {
-    // message.reply('pong');
     message.channel.sendMessage('pong');
   }
 });
+
+
+
 
 bot.login('MjkzNjM5OTQ1OTg4NjAzOTA1.C7Oxcg.Hyctwf3FiJS7sndFZ3IYGBugtLM');
